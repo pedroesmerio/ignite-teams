e75d03ab8b4ffffb8d7070a8e327dce05c7e76e3
Pedro esmerio <pedro.esmerio@gatec.com.br>
2022-10-06 10:47:13 -0300

    MileStone: Create Group Screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,29 @@
+import { Button } from "@components/Button";
+import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
-import React from "react";
+import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="Jogue com sua turma" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
